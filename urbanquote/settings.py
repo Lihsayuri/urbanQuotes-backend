a116c14d120c9e856e80e9ce550ff35d90771fb2
Lihsayuri <liviasm1@al.insper.edu.br>
2021-11-11 10:36:05 -0300

    Configurando o Heroku!

--- urbanquote/settings.py
+++ urbanquote/settings.py
@@ -146,6 +146,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
